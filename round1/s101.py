5a36a8b8b837f8d65b1abc71f2299369a191c092
limk <1144650984@qq.com>
2020-02-25 23:01:51 +0800

    add 101 102 104 105

---
+++ round1/s101.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+
+    def isSymmetric(self, root):
+        def isMirror(t1, t2):
+            if t1 is None and t2 is None:
+                return True
+            if t1 is None or t2 is None:
+                return False
+            return (t1.val == t2.val) and isMirror(t1.right, t2.left) and isMirror(t1.left, t2.right)
+
+        return isMirror(root, root)
